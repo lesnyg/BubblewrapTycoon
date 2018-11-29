8f26ac6f87c5c72dfcdad9976f496d0483952da5
lesnyg <44351753+lesnyg@users.noreply.github.com>
2018-11-30 00:39:54 +0900

    commit1

--- app/src/main/java/com/example/edu/bubblewraptycoon/GameActivity.java
+++ app/src/main/java/com/example/edu/bubblewraptycoon/GameActivity.java
@@ -1,19 +1,25 @@
 package com.example.edu.bubblewraptycoon;
 
+import android.media.MediaPlayer;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
 public class GameActivity extends AppCompatActivity implements View.OnClickListener {
     ProgressBar progressBar;
-    ImageButton btnBubble1,btnBubble2,btnBubble3,btnBubble4,btnBubble5,btnBubble6,btnBubble7,btnBubble8,btnBubble9;
-    int count=0,result;
+    ImageButton btnBubble1, btnBubble2, btnBubble3, btnBubble4, btnBubble5, btnBubble6, btnBubble7, btnBubble8, btnBubble9;
+    ImageView imageboom1, imageboom2, imageboom3, imageboom4, imageboom5, imageboom6, imageboom7, imageboom8, imageboom9;
+    int count = 0, result;
     Button btnReset;
     TextView textViewResult;
+    MediaPlayer mp;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,27 +51,154 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
         btnBubble9.setOnClickListener(this);
         btnReset = findViewById(R.id.btnReset);
         btnReset.setOnClickListener(this);
+        imageboom1 = findViewById(R.id.imageboom1);
+        imageboom2 = findViewById(R.id.imageboom2);
+        imageboom3 = findViewById(R.id.imageboom3);
+        imageboom4 = findViewById(R.id.imageboom3);
+        imageboom5 = findViewById(R.id.imageboom6);
+        imageboom6 = findViewById(R.id.imageboom6);
+        imageboom7 = findViewById(R.id.imageboom7);
+        imageboom8 = findViewById(R.id.imageboom7);
+        imageboom9 = findViewById(R.id.imageboom9);
 
 
     }
 
     @Override
     public void onClick(View v) {
-        if(v == btnReset){
+        final Handler handler = new Handler();
+
+        if (v == btnReset) {
             count = 0;
             progressBar.setProgress(20);
             textViewResult.setVisibility(View.GONE);
-        }else {
-            count = count + 1;
-            result = 20-count;
+        } else {
+            count = count+1;
+            result = 20 - count;
             progressBar.setProgress(result);
-            if(result == 0)
+            if (result == 0){
                 textViewResult.setVisibility(View.VISIBLE);
-                textViewResult.setText("축하합니다 당신은 스트레스가0인 건강한 사람입니다.");
+            textViewResult.setText("한판더!!");}
+            switch (v.getId()) {
+            case R.id.btnBubble1:
+                sound();
+                btnBubble1.setVisibility(View.GONE);
+                imageboom1.setVisibility(View.VISIBLE);
 
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        imageboom1.setVisibility(View.GONE);
+                        btnBubble1.setVisibility(View.VISIBLE);
+                    }
+                }, 2000);
+                break;
+            case R.id.btnBubble2:
+                sound();
+                btnBubble2.setVisibility(View.GONE);
+                imageboom2.setVisibility(View.VISIBLE);
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        imageboom2.setVisibility(View.GONE);
+                        btnBubble2.setVisibility(View.VISIBLE);
+                    }
+                }, 2000);
+                break;
+            case R.id.btnBubble3:
+                sound();
+                btnBubble3.setVisibility(View.GONE);
+                imageboom3.setVisibility(View.VISIBLE);
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        imageboom3.setVisibility(View.GONE);
+                        btnBubble3.setVisibility(View.VISIBLE);
+                    }
+                }, 2000);
+                break;
+            case R.id.btnBubble4:
+                sound();
+                btnBubble4.setVisibility(View.GONE);
+                imageboom4.setVisibility(View.VISIBLE);
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        imageboom4.setVisibility(View.GONE);
+                        btnBubble4.setVisibility(View.VISIBLE);
+                    }
+                }, 2000);
+                break;
+            case R.id.btnBubble5:
+                sound();
+                btnBubble5.setVisibility(View.GONE);
+                imageboom5.setVisibility(View.VISIBLE);
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        imageboom5.setVisibility(View.GONE);
+                        btnBubble5.setVisibility(View.VISIBLE);
+                    }
+                }, 2000);
+                break;
+            case R.id.btnBubble6:
+                sound();
+                btnBubble6.setVisibility(View.GONE);
+                imageboom6.setVisibility(View.VISIBLE);
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        imageboom6.setVisibility(View.GONE);
+                        btnBubble6.setVisibility(View.VISIBLE);
+                    }
+                }, 2000);
+                break;
+            case R.id.btnBubble7:
+                sound();
+                btnBubble7.setVisibility(View.GONE);
+                imageboom7.setVisibility(View.VISIBLE);
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        imageboom7.setVisibility(View.GONE);
+                        btnBubble7.setVisibility(View.VISIBLE);
+                    }
+                }, 2000);
+                break;
+            case R.id.btnBubble8:
+                sound();
+                btnBubble8.setVisibility(View.GONE);
+                imageboom8.setVisibility(View.VISIBLE);
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        imageboom8.setVisibility(View.GONE);
+                        btnBubble8.setVisibility(View.VISIBLE);
+                    }
+                }, 2000);
+                break;
+            case R.id.btnBubble9:
+                sound();
+                btnBubble9.setVisibility(View.GONE);
+                imageboom9.setVisibility(View.VISIBLE);
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        imageboom9.setVisibility(View.GONE);
+                        btnBubble9.setVisibility(View.VISIBLE);
+                    }
+                }, 2000);
+                break;
 
         }
 
+        }
+    }
+
+
+    public void sound() {
+        mp = new MediaPlayer().create(this, R.raw.bubble3);
+        mp.start();
     }
 
 }
